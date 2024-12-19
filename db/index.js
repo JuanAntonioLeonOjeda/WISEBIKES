1ad1246525127e47f3fbbc663a6993afb3990ccf
JuanAntonioLeonOjeda <jalojr@hotmail.com>
2024-12-19 09:34:06 +0000

    DB_PORT

--- db/index.js
+++ db/index.js
@@ -5,6 +5,7 @@ const connection = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASS,
   {
+    port: process.env.DB_PORT,
     host: process.env.DB_URL,
     dialect: process.env.DB_DIALECT,
     logging: false,
